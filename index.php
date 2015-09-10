7da1f0252039ea1947528315da8fa5530cd46fae
telwell <me@trevorelwell.com>
2015-09-10 18:38:54 -0400

    changing spacing on table paragraph

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
 
 			<section id="table" class="col-sm-6 col-sm-offset-3">
 				<h2>Table Statistics</h2>
-				<p>Per the spec let's display the information we've saved in<br>
-						a handy table!</p>
+				<p>Per the spec let's display the information we've saved in a handy table!</p>
 				<!-- Render the table containing the various times and temps
 							which we've saved. -->
 				<?php require('inc/temp_table.php'); ?>
