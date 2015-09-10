ab9e97160f829cad595f3c76e120b801a1fcf97b
telwell <me@trevorelwell.com>
2015-09-09 23:17:23 -0400

    changing DB name in script.php

--- inc/insert_temp_db.php
+++ inc/insert_temp_db.php
@@ -4,7 +4,7 @@
 	$pdo = generate_pdo();
 
 	// Prepares and executes the passed query on the PDO.
-  $query = query_pdo($pdo, 'INSERT INTO interval(temp, created_at)
+  $query = query_pdo($pdo, 'INSERT INTO temps(temp, created_at)
 	    											VALUES(:temp, :created_at)');
 		
 	$query->execute(array(
